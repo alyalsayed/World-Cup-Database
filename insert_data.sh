25952717cf38e06e9ea5fb67393ab274b1e65281
alyalsayed <alyalsayed56@yahoo.com>
2024-01-24 17:18:28 +0200

    upload insert_data.sh file

---
+++ insert_data.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+$PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY;"
+
+
+# --------------------------------------------------------------
+# Read the CSV file and insert unique team names excluding the header names
+tail -n +2 games.csv | while IFS="," read -r year round winner opponent winner_goals opponent_goals
+do
+    # Insert unique team names into the teams table
+    $PSQL "INSERT INTO teams (name) VALUES ('$winner') ON CONFLICT (name) DO NOTHING;"
+    $PSQL "INSERT INTO teams (name) VALUES ('$opponent') ON CONFLICT (name) DO NOTHING;"
+
+    # Retrieve team IDs for the winner and opponent
+    winner_id=$($PSQL "SELECT team_id FROM teams WHERE name = '$winner';")
+    opponent_id=$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent';")
+
+    # Insert game information into the games table
+    $PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ('$year', '$round', '$winner_id', '$opponent_id', '$winner_goals', '$opponent_goals');"
+
+done
+
